f0777dffb25314734c592662e28cc3d842eb1f9c
Carlos Tactuk <ctactuk@gmail.com>
2015-04-04 08:47:19 -0400

    Addin design to the on/off actions'

--- index.php
+++ index.php
@@ -14,20 +14,39 @@ $puertos = array(
         "Puerto 8" => 22
 );
 
+echo '<div class="row">';
 foreach($puertos as $key=>$puerto){
-    echo "{$key} &nbsp;&nbsp;";
+    /*echo "{$key} &nbsp;&nbsp;";
     echo '<button type="button" class="on btn btn-success" id="'.$puerto.'"/>ON</button>&nbsp;&nbsp;';
-    echo '<button type="button" class="off btn btn-danger" id="'.$puerto.'"/>OFF</button><br/><br/>';
+    echo '<button type="button" class="off btn btn-danger" id="'.$puerto.'"/>OFF</button><br/><br/>';*/
+    //http://dummyimage.com/171x180/27a354/ffffff&text=Status:+ON
+
+    echo '
+      <div class="col-sm-6 col-md-4" style="width:230px;">
+        <div class="thumbnail">
+          <img data-src="holder.js/171x180" alt="171x180" class="'.$puerto.'" src="http://dummyimage.com/171x180/e02121/ffffff&text=Status:+OFF" data-holder-rendered="true" style="height: 180px; width: 171px; display: block;">
+          <div class="caption">
+            <h3 id="thumbnail-label">'.$key.'<a class="anchorjs-link" href="#thumbnail-label"><span class="anchorjs-icon"></span></a></h3>
+            <p><button type="button" class="on btn btn-success" id="'.$puerto.'"/>Encender</button> <button type="button" class="off btn btn-danger" id="'.$puerto.'"/>&nbsp;Apagar&nbsp;</button></p>
+          </div>
+        </div>
+      </div>
+      ';
+
 }
+echo '</div>';
 ?>
+
 <script>
 $(document).ready(function(){
     $('.on').click(function(){
         _executeCmd($(this).attr('id'), "on");
+        $('.'+$(this).attr('id')).attr("src", "http://dummyimage.com/171x180/27a354/ffffff&text=Status:+ON");
     });
 
     $('.off').click(function(){
         _executeCmd($(this).attr('id'), "off");
+        $('.'+$(this).attr('id')).attr("src", "http://dummyimage.com/171x180/e02121/ffffff&text=Status:+OFF");
     });
     
     function _executeCmd(port, cmd){
