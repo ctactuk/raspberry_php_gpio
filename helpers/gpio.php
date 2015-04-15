24864520582b5de8503c66ca80a9db4e15abc458
Carlos Tactuk <ctactuk@gmail.com>
2015-04-15 14:21:23 -0400

    Update gpio.php

--- helpers/gpio.php
+++ helpers/gpio.php
@@ -1,11 +1,21 @@
 <?php
+    include 'controllers/puerto_controller.php';
+    
     class gpio{
+        private $puerto_cl = new puerto();
         public gpio(){
             
         }
         
-        public function executeCommand($port, $command){
-            exec('');
+        public function executeCommand($port, $cmd){
+            $resultado = "";
+            $estado = $cmd == 'on' ? 1 : 0;
+            
+            $return = exec('sudo python /home/pi/domotica/command.py ' . $port . ' ' . $cmd , $output);
+    
+            $puertos = $puerto_cl->updatePuertos('puerto', array('puerto' => $port), array('encendido' => $estado));
+            
+            return $output;
         }
     }
 ?>
