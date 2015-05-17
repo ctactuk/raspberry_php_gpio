e50cc534c6195de4043d028a80eede76e698d1e4
Carlos Tactuk <ctactuk@gmail.com>
2015-05-17 08:09:12 -0400

    adding constants.php to the project

--- exec.php
+++ exec.php
@@ -14,11 +14,12 @@ if(isset($_POST['port']) && isset($_POST['cmd'])){
         $cmd = $_POST['cmd'];
         $estado = $cmd == 'on' ? 1 : 0;
         $resultado = "";
-        $return = exec( 'sudo python /home/pi/domotica/command.py' . $port . ' ' . $cmd , $output);
+        $return = exec('sudo python /home/pi/domotica/command.py' . $port . ' ' . $cmd , $output);
     
         $puertos = $puerto_cl->updatePuertos('puerto', array('puerto' => $port), array('encendido' => $estado));
 
-        echo json_encode($output);
+        //echo json_encode($output);
+        echo json_encode('sudo python /home/pi/domotica/command.py' . $port . ' ' . $cmd);
 }
 
 ?>
