e4901369ab1e4fc769afba3a453fea874717f991
Carlos Tactuk <ctactuk@gmail.com>
2015-05-17 08:29:16 -0400

    adding constants.php to the project

--- exec.php
+++ exec.php
@@ -1,7 +1,7 @@
 <?php
 echo json_encode($_POST);
 include 'config/app_config.php'
-include 'controllers/puerto_controller.php';
+//include 'controllers/puerto_controller.php';
 /*
 define('PATH_TO_SCRIPT','/home/pi/domotica/command.py');
 define('ROOT_PRIVILEGES','sudo');
@@ -16,7 +16,7 @@ if(isset($_POST['port']) && isset($_POST['cmd'])){
         $resultado = "";
         $return = exec('sudo python /home/pi/domotica/command.py' . $port . ' ' . $cmd , $output);
     
-        $puertos = $puerto_cl->updatePuertos('puerto', array('puerto' => $port), array('encendido' => $estado));
+        //$puertos = $puerto_cl->updatePuertos('puerto', array('puerto' => $port), array('encendido' => $estado));
 
         echo json_encode($output);
         //echo json_encode($_POST);
